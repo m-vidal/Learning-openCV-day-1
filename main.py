cf90850d0623e6821ce4307153242b27196ead87
m-vidal <vidal001eme@gmail.com>
2026-02-27 08:30:01 +0000

    adicionando as dependências do projeto e o teste da streamlit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello!!!!")
+import cv2
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+@st.cache_resource
+def load_model():
+    net = cv2.dnn.readNetFromCaffe(
+        "model/deploy.prototxt",
+        "model/res10_300x300_ssd_iter_140000.caffemodel"
+    )
+    return (net)
+
+net = load_model()

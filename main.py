d2f673374ad320eae1373c1da7062e39547dae0b
m-vidal <vidal001eme@gmail.com>
2026-03-02 23:46:32 +0000

    adicionando o loop para verificar os rostos e dezenhar a box em volta

--- main.py
+++ main.py
@@ -15,9 +15,35 @@ net = load_model()
 
 st.title("Face Detector")
 st.write("ResNet Model + SSD — OpenCV v3.3")
-frame = st.camera_input("Camera")
+in_frame = st.camera_input("Camera")
 
-if frame is not None:
-    image_pil = Image.open(frame)
+if in_frame is not None:
+    image_pil = Image.open(in_frame)
     frame = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
-    st.image(frame)
+
+    height, width = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(
+        frame,
+        scalefactor=1.0,
+        size=(300, 300),
+        mean=(104.0, 177.0, 123.0),
+        swapRB=False
+    )
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    for i in range(detections.shape[2]):
+        trust = detections[0, 0, i, 2]
+
+        if trust < 0.5:
+            continue
+        
+        box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+        x1, y1, x2, y2 = box.astype("int")
+
+        cv2.rectangle(frame_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        
+    st.image(frame_rgb, caption="Result", width=700)
